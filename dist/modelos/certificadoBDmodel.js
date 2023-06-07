b1ef715184da2b49e0ab8e0995885e80eeb48ae9
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-06-07 19:29:16 -0400

    Se agrega modelo del certificado y su simil de .js

--- dist/modelos/certificadoBDmodel.js
+++ dist/modelos/certificadoBDmodel.js
@@ -1,2 +1,29 @@
 "use strict";
-/* crear simil de  modelos/certificado.js */ 
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Certificado = void 0;
+const mongoose_1 = require("mongoose");
+/* crear simil de  modelos/certificado.js */
+const estructuraCertificado = new mongoose_1.Schema({
+    titulo: {
+        type: String,
+        required: [true, 'Debe ingresar un titulo de certificado']
+    },
+    descripcion: {
+        type: String,
+        required: [true, 'El cuerpo del certificado no puede ser vacio']
+    },
+    motivo: {
+        type: String,
+        default: 'Certificado para acreditacion o postulacion beneficios'
+    },
+    fechaemision: {
+        //se debera implementar un catch date, este es provisional
+        type: Date,
+        required: [true, 'Se debe emitir en una fecha']
+    },
+    comunidad: [{
+            type: mongoose_1.Schema.Types.ObjectId,
+            ref: 'Comunidad'
+        }]
+});
+exports.Certificado = (0, mongoose_1.model)('Certificado', estructuraCertificado);
