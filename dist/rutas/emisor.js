4849ad8e6725ea6ccaaaaf7ef0be0ed42c36e3db
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-06-16 02:58:45 -0400

    cambios pequeños

--- dist/rutas/emisor.js
+++ dist/rutas/emisor.js
@@ -5,6 +5,19 @@ const emitirBDmodel_1 = require("../modelos/emitirBDmodel");
 const rutasEmisor = (0, express_1.Router)();
 rutasEmisor.post('/solicitud', (req, res) => {
     //validaciones
+    var caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
+    if (caracteres.test(req.body.motivo) == false) {
+        return res.json({
+            ok: false,
+            mensaje: 'Caracteres invalidos en mensaje'
+        });
+    }
+    if (req.body.motivo == '') {
+        return res.json({
+            ok: false,
+            mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
+        });
+    }
     const dataEmisor = {
         nombre: req.body.nombre,
         rut: req.body.rut,
--- rutas/emisor.ts
+++ rutas/emisor.ts
@@ -5,6 +5,23 @@ const rutasEmisor = Router();
 
 rutasEmisor.post('/solicitud', (req: any, res: Response) => {
     //validaciones
+    var caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
+
+    if(caracteres.test(req.body.motivo) == false)
+    {
+        return res.json({
+            ok: false,
+            mensaje: 'Caracteres invalidos en mensaje'
+         });
+    }
+
+    if(req.body.motivo == '')
+    {
+        return res.json({
+            ok:false,
+            mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
+        });
+    }
 
     const dataEmisor = {
         nombre: req.body.nombre,
