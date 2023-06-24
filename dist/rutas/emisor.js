41bd29299e5d81eef89dfcc786368aeda0611acd
NormanVC <norman.vergara1901@alumnos.ubiobio.cl>
2023-06-24 12:05:14 -0400

    Se crea metodo para crear solicitud para emitir certificad, con validacion de usuario que pertenece a la comunidad

--- dist/rutas/emisor.js
+++ dist/rutas/emisor.js
@@ -12,50 +12,69 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const emitirBDmodel_1 = require("../modelos/emitirBDmodel");
 const autenticacion_1 = require("../middlewares/autenticacion");
+const usuarioBDModel_1 = require("../modelos/usuarioBDModel"); // Importar el modelo de Usuario
 const rutasEmisor = (0, express_1.Router)();
-rutasEmisor.post('/solicitud', [autenticacion_1.verificaToken], (req, res) => {
-    const body = req.body;
-    body.usuario = req.usuario._id;
-    body.comunidad = req.usuario.comunidad;
-    //validaciones
-    var caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
-    if (caracteres.test(req.body.motivo) == false) {
-        return res.json({
-            ok: false,
-            mensaje: 'Caracteres invalidos en mensaje'
-        });
-    }
-    if (req.body.motivo == '') {
-        return res.json({
-            ok: false,
-            mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
-        });
-    }
-    //agregar validacion con token de usuario
-    // se debe hacer una funcion para revisar si existe una solicitud del usuario
-    const dataEmisor = {
-        nombre: req.body.nombre,
-        rut: req.body.rut,
-        motivo: req.body.motivo,
-        fechaemision: req.body.date,
-        certficado: req.body.certificado,
-        comunidad: req.body.comunidad,
-        estado: req.body.estado
-    };
-    emitirBDmodel_1.Emisor.create(body).then((emisorBD) => __awaiter(void 0, void 0, void 0, function* () {
+rutasEmisor.post('/solicitud', [autenticacion_1.verificaToken], (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const usuarioId = req.usuario._id;
+    const comunidadId = req.body.comunidad;
+    try {
+        // Buscar al usuario en la base de datos
+        const usuario = yield usuarioBDModel_1.Usuario.findById(usuarioId);
+        if (!usuario) {
+            return res.status(404).json({
+                ok: false,
+                mensaje: 'Usuario no encontrado'
+            });
+        }
+        //Se Verifica si el usuario pertenece a la comunidad especificada
+        const perteneceComunidad = usuario.comunidad.some((comunidad) => comunidad.toString() === comunidadId);
+        if (!perteneceComunidad) {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'El usuario no pertenece a la comunidad especificada'
+            });
+        }
+        const body = req.body;
+        body.usuario = usuarioId;
+        body.comunidad = comunidadId;
+        // Validaciones
+        const caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
+        if (!caracteres.test(req.body.motivo)) {
+            return res.json({
+                ok: false,
+                mensaje: 'Caracteres inválidos en el mensaje'
+            });
+        }
+        if (req.body.motivo === '') {
+            return res.json({
+                ok: false,
+                mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
+            });
+        }
+        const dataEmisor = {
+            nombre: req.body.nombre,
+            rut: req.body.rut,
+            motivo: req.body.motivo,
+            fechaemision: req.body.date,
+            certficado: req.body.certificado,
+            comunidad: req.body.comunidad,
+            estado: req.body.estado
+        };
+        const emisorBD = yield emitirBDmodel_1.Emisor.create(body);
         yield emisorBD.populate({ path: 'usuario', select: '-password' });
         yield emisorBD.populate({ path: 'comunidad' });
         res.json({
             ok: true,
             dataEmisor
         });
-    })).catch(err => {
-        res.json({
+    }
+    catch (err) {
+        res.status(500).json({
             ok: false,
             err
         });
-    });
-});
+    }
+}));
 //funcion para ver las solicitudes  a usuario privilegiado
 rutasEmisor.get('/solicitudes', (req, res) => {
 });
--- rutas/emisor.ts
+++ rutas/emisor.ts
@@ -1,64 +1,81 @@
-import { Router , Request , Response} from "express";
+import { Router, Request, Response } from "express";
 import { Emisor } from "../modelos/emitirBDmodel";
 import { verificaToken } from "../middlewares/autenticacion";
+import { Usuario } from "../modelos/usuarioBDModel";// Importar el modelo de Usuario
+
 const rutasEmisor = Router();
 
-rutasEmisor.post('/solicitud',[verificaToken], (req: any, res: Response) => {
-    
-    const body= req.body;
-    body.usuario = req.usuario._id;
-    body.comunidad = req.usuario.comunidad;
-    
-    //validaciones
-    var caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
+rutasEmisor.post('/solicitud', [verificaToken], async (req: any, res: Response) => {
+            const usuarioId = req.usuario._id;
+            const comunidadId = req.body.comunidad;
 
-    if(caracteres.test(req.body.motivo) == false)
-    {
-        return res.json({
-            ok: false,
-            mensaje: 'Caracteres invalidos en mensaje'
-         });
+         try {
+             // Buscar al usuario en la base de datos
+             const usuario = await Usuario.findById(usuarioId);
+
+            if (!usuario) {
+                    return res.status(404).json({
+                        ok: false,
+                        mensaje: 'Usuario no encontrado'
+                     });
+                 }
+
+            //Se Verifica si el usuario pertenece a la comunidad especificada
+             const perteneceComunidad = usuario.comunidad.some((comunidad: string) => comunidad.toString() === comunidadId);
+
+             if (!perteneceComunidad) {
+                    return res.status(403).json({
+                            ok: false,
+                            mensaje: 'El usuario no pertenece a la comunidad especificada'
+                         });
+                 }
+
+
+            const body = req.body;
+            body.usuario = usuarioId;
+            body.comunidad = comunidadId;
+
+             // Validaciones
+            const caracteres = /(^[A-Za-zÁÉÍÓÚáéíóúñÑ0-9¡!?¿@-_.,/()= ]{1,250})+$/g;
+
+             if (!caracteres.test(req.body.motivo)) {
+                 return res.json({
+                     ok: false,
+                     mensaje: 'Caracteres inválidos en el mensaje'
+                     });
+             }
+
+              if (req.body.motivo === '') {
+                 return res.json({
+                      ok: false,
+                     mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
+                    });
     }
 
-    if(req.body.motivo == '')
-    {
-        return res.json({
-            ok:false,
-            mensaje: 'Se debe proporcionar un motivo para emitir el certificado'
-        });
-    }
+                const dataEmisor = {
+                nombre: req.body.nombre,
+                rut: req.body.rut,
+                motivo: req.body.motivo,
+                fechaemision: req.body.date,
+                certficado: req.body.certificado,
+                comunidad: req.body.comunidad,
+                estado: req.body.estado
+                };
 
-    //agregar validacion con token de usuario
+    const emisorBD = await Emisor.create(body);
+    await emisorBD.populate({ path: 'usuario', select: '-password' });
+    await emisorBD.populate({ path: 'comunidad' });
 
-    // se debe hacer una funcion para revisar si existe una solicitud del usuario
-
-    const dataEmisor = {
-        nombre: req.body.nombre,
-        rut: req.body.rut,
-        motivo: req.body.motivo,
-        fechaemision: req.body.date,
-        certficado:   req.body.certificado,
-        comunidad:    req.body.comunidad,
-        estado: req.body.estado
-    }
-    
-    Emisor.create(body).then( async emisorBD =>
-        {
-            await emisorBD.populate({path:'usuario',select: '-password'})
-            await emisorBD.populate({path: 'comunidad'})
-
-            res.json({
-                ok: true,
-                dataEmisor
-            });
-        }).catch(err => {
-            res.json({
-                ok: false,
-                err
-            });
-        })
-
-
+    res.json({
+      ok: true,
+      dataEmisor
+    });
+  } catch (err) {
+    res.status(500).json({
+        ok: false,
+        err
+    });
+  }
 });
 
 //funcion para ver las solicitudes  a usuario privilegiado
